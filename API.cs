6bcea58d1945faf027e8acbdf0d8feae2bc67843
berkcancabuk <80630726+berkcancabuk@users.noreply.github.com>
2022-04-21 23:59:13 +0300

    a

--- API.cs
+++ API.cs
@@ -7,12 +7,15 @@ using System.IO;
 using System;
 public class API : MonoBehaviour
 {
-    const string BundleFolder = "http://www.ndgstudio.com.tr/berkcan/";
-    public List<string> LoadingObjectValue = new List<string>();
-    public AssetBundle bundle;
-    string bundleURL;
-    public UnityWebRequest www;
+    const string BundleFolder = "Sanal sunucu url adresi" /*Örnek: "http://www.ndgstudio.com.tr/berkcan/"*/
+        public List<string> LoadingObjectValue = new List<string>();
+    /*yüklenecek objelerimizin listesi*/
     public List<Hash128> listOfCachedVersions = new List<Hash128>();
+    /*yüklenecek objelerin version numaraları */
+    public AssetBundle bundle; /*çekilecek assetbundle ismi*/
+    string bundleURL; /* dosyalarımız içindeki verdiğimiz AssetBundleName dosyamızın ismini aratmak için bir string tanımlıyoruz*/
+    public UnityWebRequest www; /* Web request atmamız için gerekli olan www adında tanımlandı.*/
+
     public void GetBundleObject(UnityAction<GameObject> callback, Transform bundleParent)
     {
         StartCoroutine(GetDisplayBundleRoutine(callback, bundleParent));
@@ -22,59 +25,74 @@ public class API : MonoBehaviour
     {
         for (int i = 0; i < listOfCachedVersions.Count; i++)
         {
-            bundleURL = BundleFolder + LoadingObjectValue[i];
+
+            /*verdiğimiz objenin adını üstteki bundle folder yani dosyanın konumu
+            verdiğimiz assetbundleName'i giriyoruz*/
+            (örnek1: 1 - Android örnek2: Berkcan - Android)
+
+        bundleURL = BundleFolder + LoadingObjectValue[i];
+        /* burda platformu tanımlayıp sonuna android veya ios sa eklemesi için verilen bir yer dosyamızın ismi 1-Android verdiysek üstte bundleURL = BundleFolder + "1-        " olarak verirsek alttaki ile "1-Android" olacaktır. Bunun için oluşturduğumuz prefab dosyasına isim verirken assetbundle kısmında isminin sonuna android veya         IOS eklememiz gerekli eğer android veya ios platform yazmasını istemiyorsak alttaki satırı commendleyip devam edebilirsiniz.*/
 
 #if UNITY_ANDROID
-            bundleURL += "Android";
+        bundleURL += "Android";
 #else
         bundleURL += "IOS";
 #endif
-            
-            www = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL);
-                yield return www.SendWebRequest();
-                print(www.downloadProgress.ToString()+"DownloadProgress");
-                bundle = DownloadHandlerAssetBundle.GetContent(www);
-                Debug.Log("Requesting bundle at " + bundleURL);
-            
-            Debug.Log("BundleUrl : " + bundleURL);
 
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                if (Caching.IsVersionCached(bundleURL, listOfCachedVersions[i]))
-                {
-                    string rootAssetPath = bundle.GetAllAssetNames()[0];
-                    GameObject arObject = Instantiate(bundle.LoadAsset(rootAssetPath) as GameObject, bundleParent);
-                    callback(arObject);
-                    
-                }
-                else if (!Caching.IsVersionCached(bundleURL, listOfCachedVersions[i]))
-                {
-                    if (bundle != null)
-                    {
-                        bundle.Unload(false);
-                    }
-                    while (!Caching.ready)
-                    yield return null;
-                    using (var wwww = WWW.LoadFromCacheOrDownload(bundleURL, listOfCachedVersions[i]))
-                    {
-                        yield return wwww;
-                        if (!string.IsNullOrEmpty(wwww.error))
-                        {
-                            Debug.Log(wwww.error);
-                            yield return null;
-                        }
-                        bundle = wwww.assetBundle;
-                        string asset = bundle.GetAllAssetNames()[0];
-                        GameObject game = Instantiate(bundle.LoadAsset(asset) as GameObject, new Vector3(0, 0, 0), Quaternion.identity);
-                        callback(game);
-                    }
-                }
-            }
 
+        /* burada bundleURL adındaki assetbundle dosyamızı www ye eşitleyip bir istek gönderiyoruz. */
+
+        www = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL);
+        yield return www.SendWebRequest();
+        /*www adında işleyici varlık paketini indirip bundle'a eşitliyoruz*/
+        bundle = DownloadHandlerAssetBundle.GetContent(www);
+        Debug.Log("Requesting bundle at " + bundleURL);
+
+    }
+    Debug.Log("BundleUrl : " + bundleURL);
+        /* eğer internete bağlı değilse olacaklar veya dosyaya ulaşamazsa */
+        if (www.isNetworkError || www.isHttpError)
+        {
+        Debug.Log(www.error);
         }
+        else
+{
+    /* eğer dosyaya ulaşırsa olacaklar*/
+
+    /* alttaki ifte sizden dosya uzantısının adını ve version numarasını istiyor bunun için BundleURL ve benim listemdeki version numaralarını listeden giriyorum.         Version numarası için AssetBundleName.manifest dosyasının içindeki AssetFileHash: Altındaki Hash: kodunu kopyalayıp koyuyoruz.*/
+    if (Caching.IsVersionCached(bundleURL, listOfCachedVersions[i]))
+    {
+        string rootAssetPath = bundle.GetAllAssetNames()[0];
+        GameObject arObject = Instantiate(bundle.LoadAsset(rootAssetPath) as GameObject, bundleParent); callback(arObject);
+    }
+    /* Eğer bu version numarası cache de yoksa buraya giriyor */
+    else if (!Caching.IsVersionCached(bundleURL, listOfCachedVersions[i]))
+    {
+        /* bundle unload yapıyoruz.*/
+        if (bundle != null)
+        {
+            bundle.Unload(false);
+        }
+        while (!Caching.ready)
+            yield return null;
+        /* burada ise cacheden veya dosyayı direkt olduğu konumdan indirip oyunumuza instantiate ediyoruz. */
+        using (var wwww = WWW.LoadFromCacheOrDownload(bundleURL, listOfCachedVersions[i]))
+        {
+            yield return wwww;
+            if (!string.IsNullOrEmpty(wwww.error))
+            {
+                Debug.Log(wwww.error);
+                yield return null;
+            }
+            bundle = wwww.assetBundle;
+            string asset = bundle.GetAllAssetNames()[0];
+            GameObject game = Instantiate(bundle.LoadAsset(asset) as GameObject, new Vector3(0, 0, 0), Quaternion.identity);
+            callback(game);
+        }
+
     }
 }
+
+        }
+        }
+        }
